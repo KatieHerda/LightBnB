6bc6e0759a69a5b7d2f1ca16f8be825725465cbb
Katie <katie.herda2@gmail.com>
2021-10-14 23:51:13 +0000

    Refactored addProperty function to return a promise to the saved version of the new property to the database with the new proproperty form is filled out on website.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -170,7 +170,7 @@ const getAllProperties = (options, limit = 10) => {
   queryString += `
   GROUP BY 
     properties.id
-  `
+  `;
 
   // // HAVING
   //needs to be a HAVE statement to accept avg function
@@ -204,9 +204,59 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const {
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms
+  } = property;
+
+  let queryString = `
+  INSERT INTO properties (
+    owner_id,
+    title,
+    description, 
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms )
+  VALUES
+  ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;`;
+
+  return pool
+    .query(queryString, [
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      street,
+      city,
+      province,
+      post_code,
+      country,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms
+    ]);
 };
 exports.addProperty = addProperty;
